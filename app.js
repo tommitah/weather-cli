3898ad4113154037a9c1015a68cbfe1140ddd1bd
tommitah <reijotahvanainen15@gmail.com>
2021-04-29 00:53:01 +0300

    Comments, formatting

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-const fetch = require("node-fetch")
+// MODULES 
+const fetch = require("node-fetch") 
 const readline = require('readline')
 const read = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
+// API URL, two pieces 
+// -> user inputs location which is pasted in the middle
 const api_url_start = "http://api.openweathermap.org/data/2.5/weather?q="
 const api_url_end = "&units=metric&APPID=cc2928d46d101dbdb1a0e1b6b9af5e54"
 
 let location = ""
 
+// FETCH, requests the data from API in json format
+// ,then outputs some.
 async function getWeather(location) {
   fetch(api_url_start + location + api_url_end)
     .then(res => res.json())
     .then(json => console.log(
-      "Weather in " + location + ":\n\t" + json.weather[0].main +
-      "\n\tTemperature: " + json.main.temp + " Celsius" +
-      "\n\tHumidity: " + json.main.humidity + " %"
+      `Weather in ${location}:
+      \t${json.weather[0].main}
+      \tTemperature: ${json.main.temp} Celsius
+      \tHumidity: ${json.main.humidity} %`
     ))
   }
 
+// the 'main'
+// the readline module reads user input from cli and makes the API call.
 read.question('SEND LOCATION: ', (answer) => {
   getWeather(answer)
   read.close()
