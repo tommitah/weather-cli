b7459306ad7aff8763537048735db5d36f2fa89b
tommitah <reijotahvanainen15@gmail.com>
2021-04-29 01:29:07 +0300

    Slight refactor, extracted log implementation.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // MODULES 
-const fetch = require("node-fetch") 
+const fetch = require("node-fetch")
 const readline = require('readline')
 const read = readline.createInterface({
   input: process.stdin,
@@ -13,18 +13,23 @@ const api_url_end = "&units=metric&APPID=cc2928d46d101dbdb1a0e1b6b9af5e54"
 
 let location = ""
 
-// FETCH, requests the data from API in json format
+// OUTPUT, prints a formatted snippet from the JSON
+function printFormatJSON(json, location) {
+  console.log(
+    `Weather in ${location}:
+      \t${json.weather[0].main}
+      \tTemperature: ${json.main.temp} Celsius
+      \tHumidity: ${json.main.humidity} %`
+  )
+}
+
+// FETCH, requests the data from API in JSON format
 // ,then outputs some.
 async function getWeather(location) {
   fetch(api_url_start + location + api_url_end)
     .then(res => res.json())
-    .then(json => console.log(
-      `Weather in ${location}:
-      \t${json.weather[0].main}
-      \tTemperature: ${json.main.temp} Celsius
-      \tHumidity: ${json.main.humidity} %`
-    ))
-  }
+    .then(json => printFormatJSON(json, location))
+}
 
 // the 'main'
 // the readline module reads user input from cli and makes the API call.
@@ -32,4 +37,3 @@ read.question('SEND LOCATION: ', (answer) => {
   getWeather(answer)
   read.close()
 })
-
