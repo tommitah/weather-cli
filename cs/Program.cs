6ec597678e80cb6ba1d2ad0f1ce8a40bba0fac41
tommitah <reijotahvanainen15@gmail.com>
2021-05-31 15:35:54 +0300

    Formatting(coc.nvim - omnisharp)

--- cs/Program.cs
+++ cs/Program.cs
@@ -3,26 +3,32 @@ using System.Threading.Tasks;
 
 namespace WeatherNator9000
 {
-  class Program {
-    static void Main(string[] args) {
-      MainAsync(args).GetAwaiter().GetResult();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        // MainAsync, so we can call async methods.
+        static async Task MainAsync(string[] args)
+        {
+            var apiKey = new APIKey();
+            string location;
+
+            Console.WriteLine("SEND LOCATION: ");
+            location = Console.ReadLine();
+            if (location == "")
+            {
+                Console.WriteLine("is not a valid search argument.");
+                return;
+            }
+
+            var weather = await APICaller.GetForecast(
+                $"{apiKey.url.Start}{location}{apiKey.url.End}"
+            );
+
+            Console.Write($"Weather in {location}: \n\t{weather.Main} \n\t{weather.Temperature} Celsius \n\t{weather.Humidity} %");
+        }
     }
-
-    // MainAsync, so we can call async methods.
-    static async Task MainAsync(string[] args) {
-      var apiKey = new APIKey();
-      string location;
-
-      Console.WriteLine("SEND LOCATION: ");
-      location = Console.ReadLine();
-      if(location == "") {
-        Console.WriteLine("is not a valid search argument.");
-        return;
-      }
-
-      var weather = await APICaller.GetForecast($"{apiKey.url.Start}{location}{apiKey.url.End}");
-
-      Console.Write($"Weather in {location}: \n\t{weather.Main} \n\t{weather.Temperature} Celsius \n\t{weather.Humidity} %");
-    }
-  }
 }
--- cs/WeatherData.cs
+++ cs/WeatherData.cs
@@ -1,14 +1,16 @@
-namespace WeatherNator9000 
+namespace WeatherNator9000
 {
-  public readonly struct WeatherData {
-    public string Main { get; }
-    public string Temperature { get; }
-    public string Humidity { get; }
+    public readonly struct WeatherData
+    {
+        public string Main { get; }
+        public string Temperature { get; }
+        public string Humidity { get; }
 
-    public WeatherData(string main, string temp, string humty) {
-      Main = main;
-      Temperature = temp;
-      Humidity = humty;
+        public WeatherData(string main, string temp, string humty)
+        {
+            Main = main;
+            Temperature = temp;
+            Humidity = humty;
+        }
     }
-  }
 }
